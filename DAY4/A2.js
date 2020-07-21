957a96be5c65b9086ce92f3dd7df5b0e4d6dc4c5
Tufel Ali <tufelq6.998@gmail.com>
2020-07-21 21:12:53 +0530

    Add files via upload

---
+++ DAY4/A2.js
@@ -0,0 +1,10 @@
+const student = {
+    name: "Helsinki",
+    //OP = Helsinki
+    age: 24,
+    //OP = 24
+    projects:{
+        diceGame: "Two player dice game using JAVASCRIPT"
+    }
+    //OP Two Player dice game using JAVASCIPT
+}
